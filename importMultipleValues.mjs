a68b9568e8ca6733540ba47ff83ac87874db9641
nagaranigoud <bs.nagarani464@gmail.com>
2024-04-29 11:30:15 +0000

    multiplevalueone

--- importMultipleValues.mjs
+++ importMultipleValues.mjs
@@ -1,4 +1,5 @@
 import {myarray, bulb, multiplyByFour} from './exportMultipleValues.mjs'
 console.log(myarray)
-console.log(bulb)
-console.log(multiplyByFour)
+console.log(bulb(type))
+console.log(multiplyByFour(4))
+
